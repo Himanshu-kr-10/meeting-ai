27ac5fe012aeece3358d11b5ee85a8dee46b92f7
Himanshu-kr-10 <himanshukumar10202@gmail.com>
2025-06-29 17:07:10 +0530

    Step 7 tRPC setup

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,23 +1,9 @@
 'use client'
 
-import { authClient } from '@/lib/auth-client'
-import { Button } from '@/components/ui/button'
-
 export const HomeView = () => {
-  const { data: session } = authClient.useSession()
-
-  if (!session) {
-    return <div>Loading...</div>
-  }
-
-  const handleSignOut = () => {
-    authClient.signOut()
-  }
 
   return (
     <div>
-      <h1>Hello {session.user.name}</h1>
-      <Button onClick={handleSignOut}>Sign Out</Button>
     </div>
   )
 }
