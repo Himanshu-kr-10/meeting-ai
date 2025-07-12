b9caf66cf5ac0a4ad5ed89430136ea5b1ddf6fe0
Himanshu-kr-10 <139107004+Himanshu-kr-10@users.noreply.github.com>
2025-07-12 14:11:59 +0530

    Agent Id Page

---
+++ src/modules/agents/ui/components/agent-id-view-header.tsx
@@ -0,0 +1,83 @@
+import Link from "next/link"
+
+import { ChevronRightIcon, TrashIcon, PencilIcon, MoreVertical } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Button } from "@/components/ui/button";
+
+interface Props {
+  agentId: string;
+  agentName: string;
+  onEdit: () => void;
+  onRemove: () => void;
+}
+
+export const AgentIdViewHeader = ({ 
+  agentId, 
+  agentName, 
+  onEdit, 
+  onRemove 
+}: Props) => {
+  return (
+    <div className="flex items-center justify-between">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild className="font-medium text-xl">
+              <Link
+                href="/agents"
+                className="text-muted-foreground hover:text-foreground"
+              >
+                My Agents
+              </Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator className="text-foreground text-xl font-medium [&>svg]:size-4">
+            <ChevronRightIcon className="w-4 h-4" />
+          </BreadcrumbSeparator>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild className="font-medium text-xl">
+              <Link
+                href={`/agents/${agentId}`}
+                className="text-muted-foreground hover:text-foreground"
+              >
+                {agentName}
+              </Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      {/* Without modal false the dialog that this dropdown opens cause the website to get stuck / freeze */}
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost">
+            <MoreVertical />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={onEdit}>
+            <PencilIcon className="size-4 text-black" />
+            Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={onRemove}>
+            <TrashIcon className="size-4 text-red-500" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  )
+}
