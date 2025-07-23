a24785ad4fab8f888e9c8adfa9729f3ac2c8cb17
Himanshu-kr-10 <139107004+Himanshu-kr-10@users.noreply.github.com>
2025-07-23 20:53:45 +0530

    Added Meeting Filters

--- src/app/(dashboard)/meetings/page.tsx
+++ src/app/(dashboard)/meetings/page.tsx
@@ -1,14 +1,23 @@
-import { MeetingsView, MeetingsViewError, MeetingsViewLoading } from '@/modules/meetings/ui/views/meetings-view'
-import { getQueryClient, trpc } from '@/trpc/server'
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 import React, { Suspense } from 'react'
-import { ErrorBoundary } from 'react-error-boundary';
-import MeetingsListHeader from '@/modules/meetings/ui/components/meetings-list-header';
 import { headers } from 'next/headers';
-import { auth } from '@/lib/auth';
 import { redirect } from 'next/navigation';
+import { ErrorBoundary } from 'react-error-boundary';
+import type { SearchParams } from 'nuqs/server';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 
-const Page = async () => {
+import { auth } from '@/lib/auth';
+import { getQueryClient, trpc } from '@/trpc/server'
+
+import { loadSearchParams } from '@/modules/meetings/params';
+import MeetingsListHeader from '@/modules/meetings/ui/components/meetings-list-header';
+import { MeetingsView, MeetingsViewError, MeetingsViewLoading } from '@/modules/meetings/ui/views/meetings-view'
+
+interface Props {
+  searchParams: Promise<SearchParams>;
+}
+
+const Page = async ({ searchParams }: Props) => {
+  const filters = await loadSearchParams(searchParams)
   const session = await auth.api.getSession({
     headers: await headers(),
   })
@@ -18,7 +27,9 @@ const Page = async () => {
   }
 
   const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.meetings.getMany.queryOptions({}));
+  void queryClient.prefetchQuery(trpc.meetings.getMany.queryOptions({
+    ...filters,
+  }));
   
   return (
     <>
--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -6,6 +6,7 @@ import { createTRPCRouter, protectedProcedure } from '@/trpc/init'
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from '@/constants'
 import { TRPCError } from '@trpc/server'
 import { createMeetingSchema, updateMeetingSchema } from '../schema'
+import { MeetingStatus } from '../types'
 
 export const meetingsRouter = createTRPCRouter({
   update: protectedProcedure
@@ -62,10 +63,20 @@ export const meetingsRouter = createTRPCRouter({
         page: z.number().default(DEFAULT_PAGE),
         pageSize: z.number().min(MIN_PAGE_SIZE).max(MAX_PAGE_SIZE).default(DEFAULT_PAGE_SIZE),
         search: z.string().nullish(),
+        agentId: z.string().nullish(),
+        status: z
+          .enum([
+            MeetingStatus.Active,
+            MeetingStatus.Cancelled,
+            MeetingStatus.Completed,
+            MeetingStatus.Processing,
+            MeetingStatus.Upcoming
+          ])
+          .nullish()
       }),
     )
     .query(async ({ ctx, input }) => {
-      const { search, page, pageSize } = input
+      const { search, page, pageSize, status, agentId } = input
       const data = await db
         .select({
           ...getTableColumns(meetings),
@@ -74,7 +85,14 @@ export const meetingsRouter = createTRPCRouter({
         })
         .from(meetings)
         .innerJoin(agents, eq(meetings.agentId, agents.id))
-        .where(and(eq(meetings.userId, ctx.auth.user.id), search ? ilike(meetings.name, `%${search}%`) : undefined))
+        .where(
+          and(
+            eq(meetings.userId, ctx.auth.user.id), 
+            search ? ilike(meetings.name, `%${search}%`) : undefined,
+            status ? eq(meetings.status, status) : undefined,
+            agentId ? eq(meetings.agentId, agentId) : undefined
+          )
+        )
         .orderBy(desc(meetings.createdAt), desc(meetings.id))
         .limit(pageSize)
         .offset((page - 1) * pageSize)
